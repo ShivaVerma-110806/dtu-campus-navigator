d4adb0f53e366d705af8bdfafcb52bf1a3cb382c
iit378417@gmail.com <your@email.com>
2026-07-13 05:41:29 +0530

    added dev ip

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 //MIDDLEWARE
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "http://localhost:5174"],
   credentials: true
 }));
 app.use(express.json());
